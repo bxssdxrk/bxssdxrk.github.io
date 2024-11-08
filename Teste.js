65bb1d8e724e22031d384b8974948e16ad835e27
Bass Dark <72193669+bxssdxrk@users.noreply.github.com>
2024-11-07 22:32:24 -0300

    Update Teste.js

--- Teste.js
+++ Teste.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 const { question, onlyNumbers } = require("./utils");
-const { isGlobal } = require("./middlewares/dxrkplusPermissions"); // Importa a função isGlobal
+const { isGlobal } = require("./middlewares/dxrkplusPermissions");
 
 const {
   default: makeWASocket,
@@ -30,10 +30,10 @@ const GROUPS_DIR = path.join(DATA_DIR, "grupos");
 const MESSAGES_DIR = path.join(DATA_DIR, "mensagens");
 const STATUS_DIR = path.join(DATA_DIR, "status");
 const GROUP_STATUS_FILE = path.resolve(__dirname, "..", "database", "groupsStatus.json");
+const USER_STATUS_PATH = path.join(__dirname, '../database/userStatus.json');
 
 const store = makeInMemoryStore({});
 
-// Função para carregar ou criar o arquivo groupsStatus.json
 function loadGroupStatus() {
   if (!fs.existsSync(GROUP_STATUS_FILE)) {
     fs.writeFileSync(GROUP_STATUS_FILE, JSON.stringify({ groups: {} }, null, 2));
@@ -41,7 +41,6 @@ function loadGroupStatus() {
   return JSON.parse(fs.readFileSync(GROUP_STATUS_FILE, "utf-8"));
 }
 
-// Função para salvar o estado de grupos
 function saveGroupStatus(groupStatus) {
   fs.writeFileSync(GROUP_STATUS_FILE, JSON.stringify(groupStatus, null, 2));
 }
@@ -142,24 +141,20 @@ async function connect() {
 
   socket.ev.on("creds.update", saveCreds);
 
-// Evento para detectar novos participantes
-socket.ev.on("group-participants.update", async ({ id, participants, action }) => {
+  socket.ev.on("group-participants.update", async ({ id, participants, action }) => {
     const groupStatus = loadGroupStatus();
 
-    // Verifica se o grupo tem boas-vindas ativadas
     const group = groupStatus.groups[id] || { boasVindas: false, boasVindasMsg: "Bem vindo(a) %user%!\n> Entrou: %date% às %time%." };
 
     if (action === "add" && group.boasVindas) {
         const now = new Date();
 
-        // Formata a data como DD/MM/YYYY
         const date = now.toLocaleDateString('pt-BR', {
             day: '2-digit',
             month: '2-digit',
             year: 'numeric',
         });
 
-        // Formata a hora como HH:MM:SS
         const time = now.toLocaleTimeString('pt-BR', {
             hour: '2-digit',
             minute: '2-digit',
@@ -175,55 +170,49 @@ socket.ev.on("group-participants.update", async ({ id, participants, action }) =
             await socket.sendMessage(id, { text: welcomeMsg, mentions: [participant] });
         }
 
-        // Verificar se o usuário é global em todos os grupos
         for (const participant of participants) {
             const isParticipantGlobal = await checkIfGlobal(participant);
 
             if (isParticipantGlobal) {
-                // Adiciona o usuário como global no grupo atual
-                if (!groupStatus.groups[id].users) {
-                    groupStatus.groups[id].users = {};
-                }
-                groupStatus.groups[id].users[participant] = { usuarioGlobal: true };
-
-                // Salva as atualizações no arquivo
-                saveGroupStatus(groupStatus);
+                setUserGlobalStatus(participant);
             }
         }
     }
 
-    // Atualiza o status do grupo no arquivo
     if (!groupStatus.groups[id]) {
         groupStatus.groups[id] = group;
         saveGroupStatus(groupStatus);
     }
-});
+  });
 
-// Função para verificar se o usuário é global
-async function checkIfGlobal(participant) {
-    const USER_STATUS_PATH = path.join(__dirname, '../database/userStatus.json');
-
-    // Lê o arquivo JSON para obter os status do usuário
+  async function checkIfGlobal(participant) {
     let userStatusData;
     try {
         userStatusData = JSON.parse(fs.readFileSync(USER_STATUS_PATH, 'utf8'));
     } catch (error) {
         console.error("Erro ao ler o arquivo de status do usuário:", error);
-        return false; // Retorna false se não for possível ler os dados
+        return false;
     }
 
-    // Verifica se o usuário é global em qualquer grupo
-    for (const groupId in userStatusData.groups) {
-        const groupUsers = userStatusData.groups[groupId].users;
-        if (groupUsers && groupUsers[participant]) {
-            if (groupUsers[participant].usuarioGlobal) {
-                return true; // O usuário é global
-            }
-        }
+    return userStatusData[participant]?.usuarioGlobal || false;
+  }
+
+  function setUserGlobalStatus(participant) {
+    let userStatusData = {};
+
+    try {
+        userStatusData = JSON.parse(fs.readFileSync(USER_STATUS_PATH, 'utf8'));
+    } catch (error) {
+        console.error("Erro ao ler o arquivo de status do usuário:", error);
     }
 
-    return false; // O usuário não é global
-}
+    if (!userStatusData[participant]) {
+        userStatusData[participant] = {};
+    }
+    userStatusData[participant].usuarioGlobal = true;
+
+    fs.writeFileSync(USER_STATUS_PATH, JSON.stringify(userStatusData, null, 2));
+  }
 
   socket.ev.on("chats.upsert", async (chats) => {
     for (const chat of chats) {
